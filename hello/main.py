70f4ed39be26d46434e6de6511bc013e13253afe
ZARIFMAC <uzbzarif@gmail.com>
2021-02-21 20:38:27 +0900

    route

---
+++ hello/main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+home_page = '''
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is Home page</h1>
+<h4>This is heading 4</h4>
+<h5>This is heading 5</h5>
+
+
+</body>
+</html>
+
+'''
+
+@app.route('/')
+def hello():
+    return 'Hi123'
+
+
+@app.route('/index')
+@app.route('/home')
+def home():
+    return home_page
+
+app.run(debug=True)
