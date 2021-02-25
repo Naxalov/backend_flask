026f7ccbea25cfcf8f77adeb6da23b58c9c498cf
ZARIFMAC <uzbzarif@gmail.com>
2021-02-25 21:45:10 +0900

    args

--- request_data/app.py
+++ request_data/app.py
@@ -3,7 +3,12 @@ from flask import Flask,request
 app = Flask(__name__)
 
 
-
+@app.route('/api')
+def api():
+    r = request.args
+    a=r['a']
+    b=r['b']
+    return {'a':a,'b':b}
 
 
 @app.route('/index')
